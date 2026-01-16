cf3b94626f456d545aedd98d10dd6e65119b7f0b
IDGS802-23002162 <86053@alumnos.utleon.edu.mx>
2026-01-15 19:07:29 -0600

    Practica  4 no habia guardado

--- practica4.py
+++ practica4.py
@@ -1,12 +1,33 @@
-
-
-x=0
+x = 0
+lista1 = []
 
 while x != 20:
-print("Digite 20 numeros al azar")
-d=int(input("Numero"+ x+1+":"))
-    lista1.insert(d)
-x+= 1
+    d = int(input("Numero " + str(x + 1) + ": "))
+    lista1.append(d)
+    x += 1
 
-if x=20:
-    print(lista1.sort())
+lista1.sort()
+print(lista1)
+
+repetidos = {}
+
+for num in lista1:
+    if num in repetidos:
+        repetidos[num] += 1
+    else:
+        repetidos[num] = 1
+
+for num, veces in repetidos.items():
+    print(str(num) + "-" + str(veces))
+
+pares = []
+impares = []
+
+for num in lista1:
+    if num % 2 == 0:
+        pares.append(num)
+    else:
+        impares.append(num)
+
+print(pares)
+print(impares)
